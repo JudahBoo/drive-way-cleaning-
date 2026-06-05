e0217e7dbb917d685206bdddc4941c9297286d4a
JudahBoo <123729ajgk@gmail.com>
2026-06-04 17:46:50 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your Google Apps Script Web App URL here after deploying.
 // It looks like: https://script.google.com/macros/s/ABC123.../exec
-const SCRIPT_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+const SCRIPT_URL = (https://script.google.com/macros/s/AKfycbygUOyHxCkK3bYvp2P7tZhymtJhp6-tUQ_Bvu48iMSeIrijyGTcl8_X3t48sO0ytJZEEg/exec)
